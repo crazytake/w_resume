bce58d4b4b4e05852936d9a917cf9362c74e0415
KIng_Kel <769692436@qq.com>
2018-01-10 22:59:24 +0800

    Admin add Resume

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -5,8 +5,8 @@ return array(
 	'DB_TYPE'   => 'mysqli', // 数据库类型
 	'DB_HOST'   => 'localhost', // 服务器地址
 	'DB_NAME'   => 'w_resume', // 数据库名
-	'DB_USER'   => 'w_resume', // 用户名
-	'DB_PWD'    => 'zhee4#%dzK', // 密码
+	'DB_USER'   => 'root', // 用户名
+	'DB_PWD'    => 'root', // 密码
 	'DB_PORT'   => 3306, // 端口
 	'DB_PREFIX' => 'kwg_', // 数据库表前缀
 	'DB_CHARSET'=> 'utf8', // 字符集
---
+++ Application/Admin/Controller/ResumeController.class.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kel
+ * Date: 2018/1/10
+ * Time: 22:58
+ */
