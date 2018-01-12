c916c9b938aa8fed58cbfa22bc03aed798e5f80b
gyingh <gyingh@outlook.com>
2018-01-12 23:54:56 +0800

    管理员列表

--- Application/Admin/Controller/AdminController.class.php
+++ Application/Admin/Controller/AdminController.class.php
@@ -3,6 +3,9 @@ namespace Admin\Controller;
 use Think\Controller;
 class AdminController extends CommonController {
     public function lst(){
+		$admin=D("AdminUserView");
+		$adminList=$admin->select();
+		$this->assign("adminList",$adminList);
 		$this->display();
     }
 	public function aaa(){
