4a791b3601769ed017da589ce32bda5721771bcc
KIng_Kel <769692436@qq.com>
2018-01-13 17:30:30 +0800

    add Admin Resuem lst

--- Application/Admin/Controller/ResumeController.class.php
+++ Application/Admin/Controller/ResumeController.class.php
@@ -5,11 +5,3 @@
  * Date: 2018/1/10
  * Time: 22:58
  */
-namespace Admin\Controller;
-use Think\Controller;
-
-class ResumeController extends CommonController{
-  public function lst(){
-    $this->display();
-  }
-}
